0ab668394e169f03f9302f62a1ead40a3c8fbd3c
Woo <rhrhghrhrhgh@ajou.ac.kr>
2025-04-28 16:43:47 +0900

    feat : 쿼리 향상

--- src/main/java/hekireki/sanjijiksong/domain/openapi/Repository/PriceDailyRepository.java
+++ src/main/java/hekireki/sanjijiksong/domain/openapi/Repository/PriceDailyRepository.java
@@ -2,6 +2,8 @@ package hekireki.sanjijiksong.domain.openapi.Repository;
 
 import hekireki.sanjijiksong.domain.openapi.entity.PriceDaily;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -16,4 +18,19 @@ public interface PriceDailyRepository extends JpaRepository<PriceDaily, Long> {
             String categoryCode, String itemCode, LocalDate startDate, LocalDate endDate);
 
     PriceDaily findTopByItemNameContainingOrderBySnapshotDateDesc(String keyword);
+
+    @Query("""
+    SELECT p
+    FROM PriceDaily p
+    WHERE
+        (:keywords) IS NULL OR
+        EXISTS (
+            SELECT 1
+            FROM TrendingKeyword tk
+            WHERE p.itemName LIKE CONCAT('%', tk.keyword, '%')
+            AND tk.keyword IN :keywords
+        )
+    ORDER BY p.snapshotDate DESC
+   """)
+    List<PriceDaily> findLatestByKeywords(@Param("keywords") List<String> keywords);
 }
